35ee89a1bb5f168f20e788bcf5269a09994cdd51
fredaugermorin <frederick.augermorin@gmail.com>
2016-05-17 23:10:18 -0400

    modified main, everything should work

--- basic_classes/Main.cpp
+++ basic_classes/Main.cpp
@@ -14,17 +14,25 @@ int main(void)
 	Event myEvent(type);
 	std::cout << myEvent.getType() << std::endl;
 
+	Event myEventDefault; //default constructor
+	myEventDefault.setType("FILL");
+	std::cout<<myEventDefault.getType()<<std::endl;
+
 	myEvent.setType("ORDER");
 	std::cout << myEvent.getType() <<std::endl;
 
-	TickEvent myTickEvent("AAPL","2016-05-17-16:00",193.56,193.59);
-	myTickEvent.getTicker();
+	TickEvent myDefTickEvent; //default constructor
+	myDefTickEvent.setType("TICK"); //inherited methods
+	std::cout<< myDefTickEvent.getType()<<std::endl;
+
+	TickEvent myTickEvent("AAPL","2016-05-17-16:00",193.56,193.59); //overloaded constructor
+	myTickEvent.getTicker(); //class specific methods
 	myTickEvent.getTime();
 	myTickEvent.getBid();
 	myTickEvent.getAsk();
 
 	std::cout <<myTickEvent.getTicker() <<std::endl<< myTickEvent.getTime()<< std::endl << myTickEvent.getBid() << std::endl << myTickEvent.getAsk()<<std::endl;
 
-	myTickEvent.setType("MARKET");
+	myTickEvent.setType("MARKET"); //inherited methods
 	std::cout << myTickEvent.getType() <<std::endl;
 }
