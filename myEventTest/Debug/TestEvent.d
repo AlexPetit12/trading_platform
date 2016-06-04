7184527daf03f052b8dbcfcb9aac0bc79d57dfb2
fredaugermorin <frederick.augermorin@gmail.com>
2016-06-04 16:29:29 -0400

    removed myEventTest test directory

--- myEventTest/Debug/TestEvent.d
+++
@@ -1,6 +0,0 @@
-TestEvent.d: ../TestEvent.cpp ../TestEvent.h \
- /home/fredaugermorin/trading_platform/EventTest/Event.h
-
-../TestEvent.h:
-
-/home/fredaugermorin/trading_platform/EventTest/Event.h:
